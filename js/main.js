711988665c1b64578ddcefd5e4e585edeb07087c
Tarei King <tareiking@gmail.com>
2014-11-07 07:58:54 +1000

    Add isotope portfolio sorting JS inside check for element on page

--- js/main.js
+++ js/main.js
@@ -40,5 +40,51 @@
     //   });
     // })
 
+    // Portfolio filter and sorting
+    // # TODO : Implement
+
+    if ( $('.portfolioContainer').length ) {
+
+      $(window).load(function(){
+
+        var $container = $('.portfolioContainer'),
+            $body = $('body'),
+            colW = 375,
+            columns = null;
+
+        $container.isotope({
+          // disable window resizing
+          resizable: true,
+          masonry: {
+            columnWidth: colW
+          }
+        });
+
+        $(window).smartresize(function(){
+          // check if columns has changed
+          var currentColumns = Math.floor( ( $body.width() -30 ) / colW );
+          if ( currentColumns !== columns ) {
+            // set new column count
+            columns = currentColumns;
+            // apply width to container manually, then trigger relayout
+            $container.width( columns * colW )
+              .isotope('reLayout');
+          }
+
+        }).smartresize(); // trigger resize to set container width
+        $('.portfolioFilter a').click(function(){
+              $('.portfolioFilter .current').removeClass('current');
+              $(this).addClass('current');
+
+              var selector = $(this).attr('data-filter');
+              $container.isotope({
+                  filter: selector,
+               });
+               return false;
+          });
+
+      });
+
+    };
 
 })(jQuery);
