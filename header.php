489833caa354fcd8983a2626a8e3cf5a4229104d
Tarei King <tareiking@gmail.com>
2014-11-06 22:30:51 +1000

    Add IE Shivs to header

--- header.php
+++ header.php
@@ -15,7 +15,16 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<!--[if IE]><style type="text/css">.pie {behavior:url(PIE.htc);}</style><![endif]-->
+
 <?php wp_head(); ?>
+
+<!--[if lt IE 9]>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/respond-1.1.0.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/html5element.js"></script>
+<![endif]-->
+
 </head>
 
 <body <?php body_class(); ?>>
