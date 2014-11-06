9b1a3b10e583186362df24302cb39608d2502713
Tarei King <tareiking@gmail.com>
2014-11-07 07:51:50 +1000

    Enqueue remaining scripts
    - Also change enqueue dependencies

--- functions.php
+++ functions.php
@@ -103,7 +103,12 @@ function tk_knight_scripts() {
 
 	wp_enqueue_script( 'tk-knight-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-	wp_enqueue_script( 'tk-knight-main', get_template_directory_uri() . '/js/main.js', array( 'classie' ), '1.0', true );
+	wp_enqueue_script( 'tk-scrolltofixed', get_template_directory_uri() . '/js/jquery-scrolltofixed.js', array( 'jquery' ), '1.0', true );
+	wp_enqueue_script( 'tk-easing', get_template_directory_uri() . '/js/jquery.easing.1.3.js', array( 'jquery' ), '1.3', true );
+	wp_enqueue_script( 'tk-isotope', get_template_directory_uri() . '/js/jquery.isotope.js', array( 'jquery' ), '1.0', true );
+	wp_enqueue_script( 'tk-wow', get_template_directory_uri() . '/js/wow.js', array( 'jquery' ), '1.0', true );
+	wp_enqueue_script( 'tk-classie', get_template_directory_uri() . '/js/classie.js', array( 'jquery', 'tk-wow' ), '1.0', true );
+	wp_enqueue_script( 'tk-knight-main', get_template_directory_uri() . '/js/main.js', array( 'tk-classie' ), '1.0', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
