e93618e610a390d9e2fdda2614e89699a6e9b789
Tarei King <tareiking@gmail.com>
2014-11-07 21:19:57 +1000

    Check $sections exists, otherwise, show normal page content partials

--- content-section.php
+++ content-section.php
@@ -7,7 +7,11 @@
 
 $sections = get_post_meta( get_the_ID(), '' . '_cmb2_repeat_group', true );
 
-	if ( $sections ) {
+if ( count ( $sections ) == 1 ) { // There is always one element in the array
+
+	get_template_part( 'content', 'page' );
+
+} else {
 
 		foreach ( (array) $sections as $key => $section ) {
 
@@ -78,8 +82,4 @@ $sections = get_post_meta( get_the_ID(), '' . '_cmb2_repeat_group', true );
 
 	<?php } ?>
 
-<?php } else { // load the normal page content
-
-	get_template_part( 'content', 'page' );
-
-} ?>
+<?php } ?>
