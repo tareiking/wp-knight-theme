5d7655f0611d46eb8f17390aa749cb1c03e7916c
Tarei King <tareiking@gmail.com>
2014-11-07 23:21:37 +1000

    Add filter list

--- content-portfolio.php
+++ content-portfolio.php
@@ -7,17 +7,20 @@
 ?>
 
 <?php
-
 	// Get uncategorized slug id
 	$uncategorized = get_cat_id( 'uncategorized' );
+
 	// Initially, lets  get a loop of posts and use their categories
 	$args = array(
 		'category__not_in'     => $uncategorized,
 		'posts_per_page'       => 10,
 		'ignore_sticky_posts'  => 'true',
+		'orderby'              => 'count',
 		);
 
 	$folio_query = new WP_Query( $args );
+
+	$folio_categories = array();
 ?>
 
 <?php if ( $folio_query->have_posts() ) : ?>
@@ -29,10 +32,31 @@
 	<div class="portfolioFilter">
 		<ul class="Portfolio-nav wow fadeIn delay-02s">
 			<li><a href="#" data-filter="*" class="current" >All</a></li>
-			<li><a href="#" data-filter=".branding" >Branding</a></li>
-			<li><a href="#" data-filter=".webdesign" >Web design</a></li>
-			<li><a href="#" data-filter=".printdesign" >Print design</a></li>
-			<li><a href="#" data-filter=".photography" >Photography</a></li>
+
+		<!-- the loop -->
+		<?php while ( $folio_query->have_posts() ) : $folio_query->the_post(); ?>
+
+			<?php
+
+			// Let's add all the categories to an array
+			$cat =  get_the_category();
+			$first_category = $cat[0]->cat_name;
+			array_push( $folio_categories, $first_category );
+			?>
+
+		<?php endwhile; ?>
+		<!-- end of the loop -->
+
+		<?php
+			//Let's make a category list from the loop results
+			$folio_categories = array_unique( $folio_categories );
+
+			foreach ($folio_categories as $cat_name ) { ?>
+				<li>
+					<a href="#" data-filter="<?php echo $cat_name; ?>"><?php echo $cat_name;  ?></a>
+				</li>
+			<?php } ?>
+
 		</ul>
 	</div>
 
@@ -70,5 +94,7 @@
 		</div>
 	</div>
 </section><!--main-section-end-->
+
+<?php wp_reset_postdata(); ?>
 
 <?php endif; ?>
