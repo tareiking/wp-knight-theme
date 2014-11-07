aa3f9b31363da76d9d45ec0841f4eb6d31bcd3bf
Tarei King <tareiking@gmail.com>
2014-11-07 20:29:46 +1000

    Get post meta from CMB's to content-section

--- content-section.php
+++ content-section.php
@@ -7,11 +7,34 @@
 
 $sections = get_post_meta( get_the_ID(), '' . '_cmb2_repeat_group', true );
 
-if ( $sections ) { ?>
+if ( $sections ) {
+
+	foreach ( (array) $sections as $key => $section ) {
+
+		$img = $title = $desc = $content = $imagealignment = '';
+
+		if ( isset( $section['title'] ) )
+			$title = esc_html( $section['title'] );
+
+		if ( isset( $section['description'] ) )
+			$desc = wpautop( $section['description'] );
+
+		if ( isset( $section['imagealignment'] ) )
+			$imagealignment = $section['imagealignment'];
+
+		if ( isset( $section['image_id'] ) ) {
+			$img = wp_get_attachment_image( $section['image_id'], 'share-pick', null, array(
+				'class' => $imagealignment,
+			) );
+		}
+		if ( isset( $section['_cmb2_content'] ) )
+			$content = $section['_cmb2_content'];
+	} ?>
 
 
+<?php
 
-<?php } else { // load the normal page content
+} else { // load the normal page content
 
 	get_template_part( 'content', 'page' );
 
