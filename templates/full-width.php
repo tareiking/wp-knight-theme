89702a668fb19ef05419612c0891469cec39d406
Tarei King <tareiking@gmail.com>
2014-11-07 21:20:24 +1000

    Change full-width template to support CMB sections

--- templates/full-width.php
+++ templates/full-width.php
@@ -6,28 +6,18 @@
  */
 
 get_header(); ?>
-	<div id="content" class="hfeed site-content container">
+
+	<div id="content" class="hfeed site-content">
 
 	<div id="primary" class="content-area">
-		<div class="col-xs-12 col-md-12">
-
 			<main id="main" class="site-main" role="main">
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'content', 'page' ); ?>
-
-					<?php
-						// If comments are open or we have at least one comment, load up the comment template
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
-					?>
+					<?php get_template_part( 'content', 'section' ); ?>
 
 				<?php endwhile; // end of the loop. ?>
 
 			</main><!-- #main -->
-
-		</div><!-- .col -->
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
