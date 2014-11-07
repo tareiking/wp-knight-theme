4ed50d575fb008097ef45bb3b0145066abeab10e
Tarei King <tareiking@gmail.com>
2014-11-07 20:43:53 +1000

    - Add left-aligned content - Update conditionals to actually work - Add a few more selectors content--description, content--wrapper, content--inner

--- content-section.php
+++ content-section.php
@@ -7,54 +7,76 @@
 
 $sections = get_post_meta( get_the_ID(), '' . '_cmb2_repeat_group', true );
 
-if ( $sections ) {
+	if ( $sections ) {
 
-	foreach ( (array) $sections as $key => $section ) {
+		foreach ( (array) $sections as $key => $section ) {
 
-		$img = $title = $desc = $content = $imagealignment = '';
+			$img = $title = $desc = $content = $imagealignment = '';
 
-		if ( isset( $section['title'] ) )
-			$title = esc_html( $section['title'] );
+			if ( isset( $section['title'] ) )
+				$title = esc_html( $section['title'] );
 
-		if ( isset( $section['description'] ) )
-			$desc = wpautop( $section['description'] );
+			if ( isset( $section['description'] ) )
+				$desc = wpautop( $section['description'] );
 
-		if ( isset( $section['imagealignment'] ) )
-			$imagealignment = $section['imagealignment'];
+			if ( isset( $section['imagealignment'] ) )
+				$imagealignment = $section['imagealignment'];
 
-		if ( isset( $section['image_id'] ) ) {
-			$img = wp_get_attachment_image( $section['image_id'], 'share-pick', null, array(
-				'class' => $imagealignment,
-			) );
-		}
-		if ( isset( $section['_cmb2_content'] ) )
-			$content = $section['_cmb2_content'];
-	} ?>
+			if ( isset( $section['image_id'] ) ) {
+				$img = wp_get_attachment_image( $section['image_id'], 'share-pick', null, array(
+					'class' => $imagealignment,
+				) );
+			}
+			if ( isset( $section['_cmb2_content'] ) )
+				$content = $section['_cmb2_content'];
+		?>
 
-<?php if ( $imagealignment == 'alignright' ) { ?>
+		<?php if ( $imagealignment == 'alignright' ) { ?>
 
-<section class="section"><!--main-section-start-->
-	<div class="container">
-		<h2 class="text-center"><?php echo $title; ?></h2>
-		<h6 class="centered"><?php echo $desc; ?></h6>
+		<section class="section content-section"><!--main-section-start-->
+			<div class="container">
+				<h2 class="text-center"><?php echo $title; ?></h2>
+				<h6 class="text-center"><?php echo $desc; ?></h6>
 
-		<div class="row">
-			<div class="col-lg-4 col-sm-6 wow fadeInLeft delay-05s">
-				<?php echo $content; ?>
+				<div class="row">
+					<div class="col-lg-4 col-sm-6 wow fadeInLeft delay-05s">
+						<?php echo $content; ?>
+					</div>
+					<figure class="col-lg-8 col-sm-6  text-right wow fadeInUp delay-02s">
+						<?php echo $img; ?>
+					</figure>
+				</div>
+
 			</div>
-			<figure class="col-lg-8 col-sm-6  text-right wow fadeInUp delay-02s">
-				<?php echo $img; ?>
-			</figure>
-		</div>
+		</section>
 
-	</div>
-</section>
+		<?php } else { ?>
 
-<?php } else { ?>
+		<section class="section content-section">
+			<div class="container">
+				<div class="row">
+					<figure class="col-lg-5 col-sm-4 wow fadeInLeft">
+						<?php echo $img; ?>
+					</figure>
+					<div class="col-lg-7 col-sm-8 featured-work">
+						<h2><?php echo $title; ?></h2>
 
+						<div class="content--wrapper wow fadeInRight delay-02s">
+							<p class="content--description">
+								<?php echo $desc; ?>
+							</p>
 
-<?php } ?>
+							<div class="content--inner wow fadeInRight delay-06s">
+								<?php echo $content; ?>
+							</div>
+						</div>
+				</div>
+			</div>
+		</section><!--end section-->
 
+		<?php } ?>
+
+	<?php } ?>
 
 <?php } else { // load the normal page content
 
