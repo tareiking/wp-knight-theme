b8ee2bd809bb130f93371e6abbb421dbbf8c4dab
Tarei King <tareiking@gmail.com>
2014-11-07 18:51:24 +1000

    Add sidebar to default page template

--- page.php
+++ page.php
@@ -11,24 +11,28 @@
  */
 
 get_header(); ?>
+	<div id="content" class="hfeed site-content container">
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<div class="col-xs-12 col-md-9">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+			<main id="main" class="site-main" role="main">
+				<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+					<?php get_template_part( 'content', 'page' ); ?>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
+					<?php
+						// If comments are open or we have at least one comment, load up the comment template
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+					?>
 
-			<?php endwhile; // end of the loop. ?>
+				<?php endwhile; // end of the loop. ?>
 
-		</main><!-- #main -->
+			</main><!-- #main -->
+
+		</div><!-- .col -->
 	</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
