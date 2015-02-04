68f9f817ff853b78c4a23583fade81605b6e7eb9
Tarei King <tareiking@gmail.com>
2015-02-04 21:47:56 +1000

    Fix php notice error

--- templates/showcase.php
+++ templates/showcase.php
@@ -24,23 +24,24 @@ get_header(); ?>
 
 				<?php
 					// Show additional page partials if chosen by user
-					$display_options = get_post_meta( get_the_ID(), '_cmb2_show_additional_sections_checkbox', true );
+					if ( $display_options = get_post_meta( get_the_ID(), '_cmb2_show_additional_sections_checkbox', true ) ) {
 
-					if ( in_array( 'show_portfolio', $display_options ) )
-						get_template_part( 'content', 'portfolio' );
-
-
-					if ( in_array( 'show_testimonials', $display_options ) )
-						get_template_part( 'content', 'testimonial' );
+						if ( in_array( 'show_portfolio', $display_options ) )
+							get_template_part( 'content', 'portfolio' );
 
 
-					if ( in_array( 'show_logobar', $display_options ) )
-						get_template_part( 'content', 'logobar' );
+						if ( in_array( 'show_testimonials', $display_options ) )
+							get_template_part( 'content', 'testimonial' );
 
 
-					if ( in_array( 'show_team', $display_options ) )
-						get_template_part( 'content', 'team' );
+						if ( in_array( 'show_logobar', $display_options ) )
+							get_template_part( 'content', 'logobar' );
 
+
+						if ( in_array( 'show_team', $display_options ) )
+							get_template_part( 'content', 'team' );
+
+					}
 				?>
 
 			</main><!-- #main -->
