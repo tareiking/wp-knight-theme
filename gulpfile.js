01c001b22315fd47925a57140261eeb0c658b95e
Tarei King <tareiking@gmail.com>
2015-03-11 21:18:44 +1000

    Add browsersync to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,26 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
 
-// Lint Task
-gulp.task('lint', function() {
-    return gulp.src('js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync({
+        proxy: "vagrant.local"
+    });
+
+    gulp.watch("sass/*.scss", ['sass']);
+    gulp.watch("./*.css").on('change', reload);
 });
 
 // Compile Our Sass
 gulp.task('sass', function() {
     return gulp.src('sass/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('.'));
+        .pipe(gulp.dest('.'))
+        .pipe(reload({stream: true}));
 });
 
 // Concatenate & Minify JS
@@ -39,5 +46,5 @@ gulp.task('watch', function() {
 });
 
 // Default Task
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'serve', 'watch']);
 gulp.task('minify', ['lint', 'sass', 'scripts', 'watch']);
