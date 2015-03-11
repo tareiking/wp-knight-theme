31e91c92a80ae57df0c52a10a86a1bee0fa2ee81
Tarei King <tareiking@gmail.com>
2015-03-11 22:13:13 +1000

    Fix archive page width

--- archive.php
+++ archive.php
@@ -9,97 +9,105 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div id="content" class="hfeed site-content container">
 
-		<?php if ( have_posts() ) : ?>
+		<section id="primary" class="content-area">
 
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-						if ( is_category() ) :
-							single_cat_title();
+			<div class="col-xs-12 col-md-9">
 
-						elseif ( is_tag() ) :
-							single_tag_title();
+				<main id="main" class="site-main" role="main">
 
-						elseif ( is_author() ) :
-							printf( __( 'Author: %s', 'tk-knight' ), '<span class="vcard">' . get_the_author() . '</span>' );
+				<?php if ( have_posts() ) : ?>
 
-						elseif ( is_day() ) :
-							printf( __( 'Day: %s', 'tk-knight' ), '<span>' . get_the_date() . '</span>' );
+					<header class="page-header">
+						<h1 class="page-title">
+							<?php
+								if ( is_category() ) :
+									single_cat_title();
 
-						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'tk-knight' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'tk-knight' ) ) . '</span>' );
+								elseif ( is_tag() ) :
+									single_tag_title();
 
-						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'tk-knight' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'tk-knight' ) ) . '</span>' );
+								elseif ( is_author() ) :
+									printf( __( 'Author: %s', 'tk-knight' ), '<span class="vcard">' . get_the_author() . '</span>' );
 
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', 'tk-knight' );
+								elseif ( is_day() ) :
+									printf( __( 'Day: %s', 'tk-knight' ), '<span>' . get_the_date() . '</span>' );
 
-						elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-							_e( 'Galleries', 'tk-knight' );
+								elseif ( is_month() ) :
+									printf( __( 'Month: %s', 'tk-knight' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'tk-knight' ) ) . '</span>' );
 
-						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', 'tk-knight' );
+								elseif ( is_year() ) :
+									printf( __( 'Year: %s', 'tk-knight' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'tk-knight' ) ) . '</span>' );
 
-						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-							_e( 'Videos', 'tk-knight' );
+								elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
+									_e( 'Asides', 'tk-knight' );
 
-						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-							_e( 'Quotes', 'tk-knight' );
+								elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
+									_e( 'Galleries', 'tk-knight' );
 
-						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-							_e( 'Links', 'tk-knight' );
+								elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
+									_e( 'Images', 'tk-knight' );
 
-						elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-							_e( 'Statuses', 'tk-knight' );
+								elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
+									_e( 'Videos', 'tk-knight' );
 
-						elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-							_e( 'Audios', 'tk-knight' );
+								elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
+									_e( 'Quotes', 'tk-knight' );
 
-						elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-							_e( 'Chats', 'tk-knight' );
+								elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
+									_e( 'Links', 'tk-knight' );
 
-						else :
-							_e( 'Archives', 'tk-knight' );
+								elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
+									_e( 'Statuses', 'tk-knight' );
 
-						endif;
-					?>
-				</h1>
-				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
-				?>
-			</header><!-- .page-header -->
+								elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
+									_e( 'Audios', 'tk-knight' );
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+								elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
+									_e( 'Chats', 'tk-knight' );
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+								else :
+									_e( 'Archives', 'tk-knight' );
 
-			<?php endwhile; ?>
+								endif;
+							?>
+						</h1>
+						<?php
+							// Show an optional term description.
+							$term_description = term_description();
+							if ( ! empty( $term_description ) ) :
+								printf( '<div class="taxonomy-description">%s</div>', $term_description );
+							endif;
+						?>
+					</header><!-- .page-header -->
 
-			<?php tk_knight_paging_nav(); ?>
+					<?php /* Start the Loop */ ?>
+					<?php while ( have_posts() ) : the_post(); ?>
 
-		<?php else : ?>
+						<?php
+							/* Include the Post-Format-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							 */
+							get_template_part( 'content', get_post_format() );
+						?>
 
-			<?php get_template_part( 'content', 'none' ); ?>
+					<?php endwhile; ?>
 
-		<?php endif; ?>
+					<?php tk_knight_paging_nav(); ?>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+				<?php else : ?>
+
+					<?php get_template_part( 'content', 'none' ); ?>
+
+				<?php endif; ?>
+
+				</main><!-- #main -->
+				
+			</div><!-- .col -->
+			
+		</section><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
