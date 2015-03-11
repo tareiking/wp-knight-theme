de45c07f4e6e6dbf63bf16877c9853a7dad4a712
Tarei King <tareiking@gmail.com>
2015-03-11 21:26:33 +1000

    Update index and page template

--- index.php
+++ index.php
@@ -14,32 +14,38 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+				
+		<div class="col-xs-12 col-md-9">
 
-		<?php if ( have_posts() ) : ?>
+			<main id="main" class="site-main" role="main">
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+				<?php if ( have_posts() ) : ?>
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+					<?php /* Start the Loop */ ?>
+					<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php endwhile; ?>
+						<?php
+							/* Include the Post-Format-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							 */
+							get_template_part( 'content', get_post_format() );
+						?>
 
-			<?php tk_knight_paging_nav(); ?>
+					<?php endwhile; ?>
 
-		<?php else : ?>
+					<?php tk_knight_paging_nav(); ?>
 
-			<?php get_template_part( 'content', 'none' ); ?>
+				<?php else : ?>
 
-		<?php endif; ?>
+					<?php get_template_part( 'content', 'none' ); ?>
 
-		</main><!-- #main -->
+				<?php endif; ?>
+
+			</main><!-- #main -->
+
+		</div>
+
 	</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
